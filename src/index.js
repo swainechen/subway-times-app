80b313080d8fa3ce92ebad302fdac59f1fedf544
Swaine Chen <swainechen@gmail.com>
2026-03-29 00:00:26 -0400

    Comment to clarify config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// This needs to get updated with the url and port for where the mta-api app is serving. For ex. http://localhost:5000/
 axios.defaults.baseURL = '';
 ReactDOM.render(
     <App />
